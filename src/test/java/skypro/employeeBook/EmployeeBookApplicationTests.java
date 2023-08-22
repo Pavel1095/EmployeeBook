67d4ddd0b2bc20179a9212908e0d0f60e2384fde
Pavel1095 <9153220501@mail.ru>
2023-08-22 22:24:08 +0300

    Initial commit

---
+++ src/test/java/skypro/employeeBook/EmployeeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro.employeeBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
