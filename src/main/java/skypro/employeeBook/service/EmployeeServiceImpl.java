c0f69ecf6db4d25e9a0fed97326d3ed6adb12a27
Pavel1095 <9153220501@mail.ru>
2023-08-23 07:12:13 +0300

    работа выполнена

---
+++ src/main/java/skypro/employeeBook/service/EmployeeServiceImpl.java
@@ -0,0 +1,62 @@
+package skypro.employeeBook.service;
+
+import org.springframework.stereotype.Service;
+import skypro.employeeBook.dto.Employee;
+import skypro.employeeBook.dto.exception.EmployeeAlreadyAddedException;
+import skypro.employeeBook.dto.exception.EmployeeNotFoundException;
+import skypro.employeeBook.dto.exception.EmployeeStorageIsFullException;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+    private List<Employee> employees;
+
+    private static final int EMPLOYEE_SIZE = 3;
+
+    public EmployeeServiceImpl() {
+        this.employees = new ArrayList<>();
+    }
+
+    @Override
+    public Employee addEmployee(String firstName, String lastName) {
+
+        if (employees.size() == EMPLOYEE_SIZE)
+            throw new EmployeeStorageIsFullException();
+
+        Employee employee = new Employee(firstName, lastName);
+
+        if(employees.contains(employee)) {
+            throw new EmployeeAlreadyAddedException();
+        }
+
+        employees.add(employee);
+
+        return employee;
+    }
+
+    @Override
+    public Employee remoteEmployee(String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+        if (!employees.remove(employee)) {
+            throw new EmployeeNotFoundException();
+        }
+        return employee;
+    }
+
+    @Override
+    public Employee getEmployee(String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+        if (!employees.contains(employee)){
+            throw new EmployeeNotFoundException();
+        }
+        return employee;
+    }
+
+    @Override
+    public Collection<Employee> findAll(){
+        return employees;
+    }
+}
